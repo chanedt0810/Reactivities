883cb0894013f7c88ff56253e15783c72569e49e
Chane <chane.dutoit@altron.com>
2020-03-12 14:06:47 +0200

    Added button funtionality to project and Loaders. I also installed uuid to generate guid for records.

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import React, { SyntheticEvent } from 'react';
+import { Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { ActivityList } from './ActivityList';
 import { ActivityDetails } from '../details/ActivityDetails';
@@ -12,6 +12,11 @@ interface IProps {
 	editMode: boolean;
 	setEditMode: (editMode: boolean) => void;
 	setSelectedActivity: (activity: IActivity | null) => void;
+	createActivity: (activity: IActivity) => void;
+	editActivity: (activity: IActivity) => void;
+	deleteActivity: (e: SyntheticEvent<HTMLButtonElement>, id: string) => void;
+	submitting: boolean;
+	target: string;
 }
 
 export const ActivityDashboard: React.FC<IProps> = ({
@@ -20,12 +25,23 @@ export const ActivityDashboard: React.FC<IProps> = ({
 	selectedActivity,
 	editMode,
 	setEditMode,
-	setSelectedActivity
+	setSelectedActivity,
+	createActivity,
+	editActivity,
+	deleteActivity,
+	submitting,
+	target
 }) => {
 	return (
 		<Grid>
 			<Grid.Column width={10}>
-				<ActivityList activities={activities} selectActivity={selectActivity} />
+				<ActivityList
+					activities={activities}
+					selectActivity={selectActivity}
+					deleteActivity={deleteActivity}
+					submitting={submitting}
+					target={target}
+				/>
 			</Grid.Column>
 			<Grid.Column width={6}>
 				{selectedActivity && !editMode && (
@@ -35,7 +51,14 @@ export const ActivityDashboard: React.FC<IProps> = ({
 						setSelectedActivity={setSelectedActivity}
 					/>
 				)}
-				{editMode && <ActivityForm setEditMode={setEditMode} />}
+				{editMode && <ActivityForm
+					key={(selectedActivity && selectedActivity.id) || 0}
+					setEditMode={setEditMode}
+					activity={selectedActivity!}
+					createActivity={createActivity}
+					editActivity={editActivity}
+					submitting={submitting}
+				/>}
 			</Grid.Column>
 		</Grid>
 	);
